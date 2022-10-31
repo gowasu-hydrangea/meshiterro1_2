c23548f903cb9c505bb08c16726e90f60c7ac25f
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-10-31 07:58:51 +0000

    [Finish] 4-1

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,8 @@
+class HomesController < ApplicationController
+  
+  # アプリケーションを完成させよう2＞1章＞トップページの作成
+  # Railsを学ぼう＞3章＞アクションを追加する
+  def top
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # アプリケーションを完成させよう2＞1章＞トップページの作成
+  root to: "homes#top"
+end
